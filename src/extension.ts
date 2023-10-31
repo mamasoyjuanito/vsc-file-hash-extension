c801432dc1b2ad6bbd3b2b3fe3cd8c456e3b62cf
Juan Manuel Meneses <com.jmenesesi@gmail.com>
2023-10-31 12:43:15 -0600

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as CryptoJS from 'crypto-js';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let onlyHash = vscode.commands.registerCommand('file-hash.get-file-hash', (resource: vscode.Uri) => {
+		console.log(resource.path);
+
+		vscode.workspace.openTextDocument(resource).then((document) => {
+			let text = document.getText();
+			console.log("Contenido", text.length);
+			let hash = CryptoJS.SHA256(text).toString(CryptoJS.enc.Hex);
+			console.log(hash);
+			vscode.env.clipboard.writeText(hash);
+			vscode.window.showInformationMessage('Hash copied to clipboard!');
+		  });
+	});
+
+	context.subscriptions.push(onlyHash);
+
+	let hashAndTrim = vscode.commands.registerCommand('file-hash.trim-and-get-file-hash', (resource: vscode.Uri) => {
+		console.log(resource.path);
+
+		vscode.workspace.openTextDocument(resource).then((document) => {
+			let text = document.getText();
+			text = text.replace(/(\r\n|\n|\r)/gm, "");
+			fs.writeFileSync(resource.fsPath, text, 'utf8');
+			console.log("Contenido", text.length);
+			let hash = CryptoJS.SHA256(text).toString(CryptoJS.enc.Hex);
+			console.log(hash);
+			vscode.env.clipboard.writeText(hash);
+			vscode.window.showInformationMessage('Hash copied to clipboard!');
+		  });
+	});
+
+	context.subscriptions.push(hashAndTrim);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
